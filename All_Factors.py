7af9b30e2b30dec68c914056132c14774dcb820b
Graham Smith <grahamas@gmail.com>
2023-07-30 10:16:41 -0700

    Remove FairRoulette

--- All_Factors.py
+++ All_Factors.py
@@ -1,20 +1,5 @@
 import random
 random.seed(0)
-class FairRoulette():
-  def __init__(self):
-    self.pockets = []
-    for i in range (1,37):
-      self.pockets.append(i)
-    self.ball = None
-    self.pocketOdds = len(self.pockets) - 1
-  def spin(self):
-    self.ball = random.choice(self.pockets)
-  def betPocket(self, pocket, amt):
-    if str(pocket) == str(self.ball):
-      return amt*self.pocketOdds
-    else: return - amt
-  def __str__(self):
-    return 'Fair Roulette'
 
 # returns True with probability p 
 def sample_probability(p):
