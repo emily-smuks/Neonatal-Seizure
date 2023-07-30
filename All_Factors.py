4bdc6a4023521b0967e98c190e9d49b78914192b
Graham Smith <grahamas@gmail.com>
2023-07-30 10:06:02 -0700

    Remove SSRI-NAS modeling (see risk factor table notes)

--- All_Factors.py
+++ All_Factors.py
@@ -22,7 +22,7 @@ def sample_probability(p):
 
 # Should probEpilepsyGivenSSRI be higher?probEpilepsyGivenAbusesOpioids
 class Baby():
-  def __init__(self, probEpilepsyGivenPreTerm=0.07, probSmokingBeforePregnancy=.099, probSmokingFirstTrimesterGivenBefore=0.751,probSmokingSecondTrimesterGivenFirst=0.861, probPretermGivenBefore=0.123,probPretermGivenFirst=0.134, probPretermGivenSecond=0.139, probAbusesOpioid=0.014, probNASGivenAbuse=0.75, probOpioidNASControl=0, probEpilepsyGivenOpioidNAS=0.065, probAlcohol=0.303, probFASDgivenAlcohol=0.077, probPretermGivenFASD=0.184, probEpilepsyGivenFASD=0.177, probSSRI=.09, probNASGivenSSRI=0.33, probPreTermControl=0.105, probEpilepsyControl=0.002, probFASDControl=0.0003):
+  def __init__(self, probEpilepsyGivenPreTerm=0.07, probSmokingBeforePregnancy=.099, probSmokingFirstTrimesterGivenBefore=0.751,probSmokingSecondTrimesterGivenFirst=0.861, probPretermGivenBefore=0.123,probPretermGivenFirst=0.134, probPretermGivenSecond=0.139, probAbusesOpioid=0.014, probOpioidNASGivenAbuse=0.75, probOpioidNASControl=0, probEpilepsyGivenOpioidNAS=0.065, probAlcohol=0.303, probFASDgivenAlcohol=0.077, probPretermGivenFASD=0.184, probEpilepsyGivenFASD=0.177, probSSRI=.09, probNASGivenSSRI=0.33, probPreTermControl=0.105, probEpilepsyControl=0.002, probFASDControl=0.0003):
 
     # haven't seen data on seizures due to simple opioid use
     self.motherAbusesOpioid = sample_probability(probAbusesOpioid)
@@ -42,15 +42,6 @@ class Baby():
       self.isPreTerm = sample_probability(probPretermGivenFASD)
     else: 
       self.isPreterm = False
-
-    if self.motherSSRI:
-      self.isNASGivenSSRI = sample_probability(probNASGivenSSRI)
-    else:
-      self.hasEpilepsy = sample_probability(probEpilepsyControl)
-    if self.isNASGivenSSRI:
-      self.hasEpilepsy = sample_probability(probEpilepsyGivenSSRINAS)
-    else:
-       self.hasEpilepsy = sample_probability(probEpilepsyControl)
     
     self.smokingBeforePregnancy = sample_probability(probSmokingBeforePregnancy)
     if self.smokingBeforePregnancy:
@@ -67,19 +58,12 @@ class Baby():
     else:
       self.isPreterm = sample_probability(probPreTermControl)
 
-
-
-
-
     if self.isPreterm:
         self.hasEpilepsy = sample_probability(probEpilepsyGivenPreTerm)
     else:
         self.hasEpilepsy = sample_probability(probEpilepsyControl)
-    
-    
-    
 
-    # does probEpilepsyGivenAbusesOpioid intersect with prob epilepsy given NAS?
+    # NOTE: assumes that all factors ADD to each other (additive model)
     probEpilepsy = probEpilepsyControl*not (self.isPreTerm or self.motherAbusesOpioids or self.hasFASD or self.motherSSRI) + \
       probEpilepsyGivenPreTerm*self.isPreTerm + \
       probEpilepsyGivenAbusesOpioids*self.motherAbusesOpioids + \
